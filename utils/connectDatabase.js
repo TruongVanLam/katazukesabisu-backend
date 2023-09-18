ee15da479b876fb99d6848a289b2499108507a4f
Nguyễn Hoàng Anh <legendarycaptains@gmail.com>
2023-09-18 15:00:24 +0000

    Modify connection string of mogoDB

--- utils/connectDatabase.js
+++ utils/connectDatabase.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+const user = process.env.DB_USR;
+const pass = process.env.DB_PASS;
+
+mongoose.connect(`mongodb+srv://${user}:${pass}@db-katazukesabisu.xr4io2a.mongodb.net/db-katazukesabisu`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
+mongoose.set('strictQuery', true);
+
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", () => {
     console.log("Database Connected");
